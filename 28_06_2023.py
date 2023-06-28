12c9968bf2827cd4c7a8a41e1a2f8686746b22c4
karol <glomsky@gmail.com>
2023-06-28 18:37:22 +0200

    Roman_To_Integer

--- 28_06_2023.py
+++ 28_06_2023.py
@@ -1,57 +1,64 @@
 
 
 def romanToInt(s):
-    r = list(s)
-    arabic_number = []
-    for i in range(len(r)):
-        if i < len(r):
-            if r[i] + r[i + 1] == 'IV':
-                arabic_number.append(4)
-                del r[i]
-                del r[i + 1]
-            if r[i] + r[i + 1] == 'IX':
-                arabic_number.append(9)
-                del r[i]
-                del r[i + 1]
-            if r[i] + r[i + 1] == 'XL':
-                arabic_number.append(40)
-                del r[i]
-                del r[i + 1]
-            if r[i] + r[i + 1] == 'XC':
-                arabic_number.append(90)
-                del r[i]
-                del r[i + 1]
-            if r[i] + r[i + 1] == 'CD':
-                arabic_number.append(400)
-                del r[i]
-                del r[i + 1]
-            if r[i] + r[i + 1] == 'CM':
-                arabic_number.append(900)
-                del r[i]
-                del r[i + 1]
-        if r[i] == 'I':
-            arabic_number.append(1)
-            del r[i]
-        if r[i] == 'V':
-            arabic_number.append(5)
-            del r[i]
-        if r[i] == 'X':
-            arabic_number.append(10)
-            del r[i]
-        if r[i] == 'L':
-            arabic_number.append(50)
-            del r[i]
-        if r[i] == 'C':
-            arabic_number.append(100)
-            del r[i]
-        if r[i] == 'D':
-            arabic_number.append(500)
-            del r[i]
-        if r[i] == 'M':
-            arabic_number.append(1000)
-            del r[i]
-    print(sum(arabic_number))
-    return (sum(arabic_number))
+    arabic_number = 0
+    i = 0
+    while i < len(s):
+        if s[i] + s[i+1] == 'IV':
+            arabic_number += 4
+            i+=2
+            continue
+        if s[i] + s[i+1] == 'IX':
+            arabic_number += 9
+            i += 2
+            continue
+        if s[i] + s[i+1] == 'XL':
+            arabic_number += 40
+            i += 2
+            continue
+        if s[i] + s[i+1] == 'XC':
+            arabic_number += 90
+            i += 2
+            continue
+        if s[i] + s[i+1] == 'CD':
+            arabic_number += 400
+            i += 2
+            continue
+        if s[i] + s[i+1] == 'CM':
+            arabic_number += 900
+            i += 2
+            continue
+
+        if s[i] == 'I':
+            arabic_number+=1
+            i += 1
+            continue
+        if s[i] == 'V':
+            arabic_number+=5
+            i += 1
+            continue
+        if s[i] == 'X':
+            arabic_number+=10
+            i += 1
+            continue
+        if s[i] == 'L':
+            arabic_number+=50
+            i += 1
+            continue
+        if s[i] == 'C':
+            arabic_number+=100
+            i += 1
+            continue
+        if s[i] == 'D':
+            arabic_number+=500
+            i += 1
+            continue
+        if s[i] == 'M':
+            arabic_number+=1000
+            i += 1
+            continue
+    print(arabic_number)
+    return (arabic_number)
 
 
-romanToInt('IV')
+romanToInt('MCMXCIV')
