74aa6273656dacfad259e4d2c55b11ed71478432
karol <glomsky@gmail.com>
2023-06-28 20:10:58 +0200

    after refactor commit of 28.06.2023

--- 28_06_2023.py
+++ 28_06_2023.py
@@ -1,64 +1,50 @@
 
+def single_string(string, index):
+    if string[index] == 'I':
+        return 1
+    if string[index] == 'V':
+        return 5
+    if string[index] == 'X':
+        return 10
+    if string[index] == 'L':
+        return 50
+    if string[index] == 'C':
+        return 100
+    if string[index] == 'D':
+        return 500
+    if string[index] == 'M':
+        return 1000
+
+def double_string(string, index):
+    if string[index] + string[index + 1] == 'IV':
+        return 4
+    if string[index] + string[index + 1] == 'IX':
+        return 9
+    if string[index] + string[index + 1] == 'XL':
+        return 40
+    if string[index] + string[index + 1] == 'XC':
+        return 90
+    if string[index] + string[index + 1] == 'CD':
+        return 400
+    if string[index] + string[index + 1] == 'CM':
+        return 900
 
 def romanToInt(s):
     arabic_number = 0
     i = 0
+    last_index = len(s) -1
     while i < len(s):
-        if s[i] + s[i+1] == 'IV':
-            arabic_number += 4
-            i+=2
-            continue
-        if s[i] + s[i+1] == 'IX':
-            arabic_number += 9
-            i += 2
-            continue
-        if s[i] + s[i+1] == 'XL':
-            arabic_number += 40
-            i += 2
-            continue
-        if s[i] + s[i+1] == 'XC':
-            arabic_number += 90
-            i += 2
-            continue
-        if s[i] + s[i+1] == 'CD':
-            arabic_number += 400
-            i += 2
-            continue
-        if s[i] + s[i+1] == 'CM':
-            arabic_number += 900
-            i += 2
-            continue
+        if i != last_index:
+            x = double_string(s, i)
+            if x is not None:
+                arabic_number += x
+                i+=2
+                continue
+        arabic_number += single_string(s, i)
+        i += 1
 
-        if s[i] == 'I':
-            arabic_number+=1
-            i += 1
-            continue
-        if s[i] == 'V':
-            arabic_number+=5
-            i += 1
-            continue
-        if s[i] == 'X':
-            arabic_number+=10
-            i += 1
-            continue
-        if s[i] == 'L':
-            arabic_number+=50
-            i += 1
-            continue
-        if s[i] == 'C':
-            arabic_number+=100
-            i += 1
-            continue
-        if s[i] == 'D':
-            arabic_number+=500
-            i += 1
-            continue
-        if s[i] == 'M':
-            arabic_number+=1000
-            i += 1
-            continue
     print(arabic_number)
     return (arabic_number)
 
 
-romanToInt('MCMXCIV')
+romanToInt('IX')
