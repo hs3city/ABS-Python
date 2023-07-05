c0f514546edd3b66b2c3e7aea097a12f0b6c7dff
karol <glomsky@gmail.com>
2023-07-05 20:32:12 +0200

    longest common prefix: test for bug from leetcode fails

--- test_05_07_2023.py
+++ test_05_07_2023.py
@@ -2,17 +2,20 @@ import unittest
 
 
 class Solution:
-    words_comparisons = 0
-
     def longestCommonPrefix(self, strs: list[str]) -> str:
-        longest_prefix = ""
-        j = 0
-        for i in range(len(strs)):
-            if i >= len(strs) - 1:
+        longest_prefix = strs[0]
+        for i in range(1, len(strs)):
+            if i >= len(strs):
                 break
-            len_first = len(strs[i])
-            len_second = len(strs[i+1])
-            min_len = min(len_first, len_second)
+            len_prefix = len(longest_prefix)
+            len_second = len(strs[i])
+            min_len = min(len_prefix, len_second)
+            for j in range(min_len):
+                if strs[i][j] == longest_prefix[j]:
+                    continue
+                else:
+                    longest_prefix = strs[i][:j]
+                    break
         return longest_prefix
 
 
@@ -22,7 +25,7 @@ class TestCommonPrefix(unittest.TestCase):
 
         result = Solution().longestCommonPrefix(x)
 
-        assert result == ""
+        self.assertEquals(result, "")
 
     def test_two_similar_words(self):
         x = ['flower', 'flour']
@@ -31,9 +34,30 @@ class TestCommonPrefix(unittest.TestCase):
 
         self.assertEquals('flo', result)
 
+    def test_second_word_completely_different_than_first(self):
+        x = ['flower', 'potato', 'bakery']
 
+        result = Solution().longestCommonPrefix(x)
+
+        self.assertEquals('', result)
+
+    def test_one_empty_word(self):
+        x = [""]
+
+        result = Solution().longestCommonPrefix(x)
+
+        self.assertEquals('', result)
+    def test_first_longer_than_latter(self):
+        x = ["ab", "a"]
+        result = Solution().longestCommonPrefix(x)
+
+        self.assertEquals('', result)
 # TODO:
 """
 1. niewydajność dla 1 testu?
 2. więcej niz 2 wyrazy
 """
+
+x = ['flower', 'potato']
+
+result = Solution().longestCommonPrefix(x)
