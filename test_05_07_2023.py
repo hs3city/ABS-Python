f9137352ea4d4493c54390496bfb8e4e3fb3b1de
karol <glomsky@gmail.com>
2023-07-05 19:50:48 +0200

    longest common prefix: try another approach

--- test_05_07_2023.py
+++ test_05_07_2023.py
@@ -13,12 +13,6 @@ class Solution:
             len_first = len(strs[i])
             len_second = len(strs[i+1])
             min_len = min(len_first, len_second)
-            while j < min_len:
-                if strs[i][j] == strs[i+1][j]:
-                    longest_prefix += strs[i][j]
-                    j += 1
-                else:
-                    break
         return longest_prefix
 
 
@@ -30,19 +24,13 @@ class TestCommonPrefix(unittest.TestCase):
 
         assert result == ""
 
-    def test_two_similar_words(self):
-        x = ['flower', 'flour']
+    # def test_two_similar_words(self):
+    #     x = ['flower', 'flour']
+    #
+    #     result = Solution().longestCommonPrefix(x)
+    #
+    #     self.assertEquals('flo', result)
 
-        result = Solution().longestCommonPrefix(x)
-
-        self.assertEquals('flo', result)
-
-    def test_breaks_after_two_different_words(self):
-        x = ['flower', 'potato', 'flour']
-
-        Solution().longestCommonPrefix(x)
-
-        self.assertEquals(Solution.words_comparisons, 1)
 
 # TODO:
 """
